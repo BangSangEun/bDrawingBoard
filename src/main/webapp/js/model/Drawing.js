642d143b013b838aa67d82729842dc97a4a441f0
sebang <sebang@hancom.com>
2016-11-08 15:22:59 +0900

    - 그림 객체 정보 저장에 대한 구조를 개선하기 위한 그림 객체 클래스 추가

---
+++ src/main/webapp/js/model/Drawing.js
@@ -0,0 +1,66 @@
+/**
+ * Drawing 객체
+ * @constructor
+ */
+define([], function() {
+    var Drawing = function() {
+        var _type,
+            _data = {},
+            _strokeStyle,
+            _lineWidth,
+            _fillStyle,
+            _imageData;
+
+        this.getType = function() {
+            return _type;
+        };
+
+        this.setType = function(type) {
+            _type = type;
+        };
+
+        this.getData = function() {
+            return _data;
+        };
+
+        this.setData = function(data) {
+            _data = data;
+        };
+
+        this.getStrokeStyle = function() {
+            return _strokeStyle;
+        };
+
+        this.setStrokeStyle = function(strokeStyle) {
+            _strokeStyle = strokeStyle;
+        };
+
+        this.getLineWidth = function() {
+            return _lineWidth;
+        };
+
+        this.setLineWidth = function(_lineWidth) {
+            _lineWidth = lineWidth;
+        };
+
+        this.getFillStyle = function() {
+            return _fillStyle;
+        };
+
+        this.setType = function(_fillStyle) {
+            _fillStyle = fillStyle;
+        };
+
+        this.getImageData = function() {
+            return _imageData;
+        };
+
+        this.setType = function(imageData) {
+            _imageData = imageData;
+        };
+    };
+
+    return Drawing;
+});
+
+
